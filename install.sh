74557bf7c51b0f46fdbfe42552401b68c2398b89
Yifei Zhang <yidadaa@qq.com>
2022-08-16 21:54:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone https://github.com/Yidadaa/Oh-My-Nvim.git ~/.config/nvim
