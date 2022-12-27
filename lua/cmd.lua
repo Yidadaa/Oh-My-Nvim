960f466c933d2fe352ac4c0f61b9e0e55272ea48
Yidadaa <yidadaa@qq.com>
2022-12-27 08:50:16 +0000

    chore: silent guess indent

--- lua/cmd.lua
+++ lua/cmd.lua
@@ -1,9 +1,9 @@
 -- Auto guess indent on save cpp files
 vim.api.nvim_create_augroup('auto_guess_indent', { clear = true })
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
   group = "auto_guess_indent",
   pattern = { "*.*" },
   callback = function()
-    vim.cmd("GuessIndent auto_cmd")
+    vim.cmd("silent GuessIndent auto_cmd")
   end
 })
