b47ec6cc37f9744efba0d84e76f6d2cb99640f72
Yidadaa <yidadaa@qq.com>
2023-01-29 09:21:42 +0000

    chore: disable key E in nvim-tree

--- lua/plugins/setup/nvim-tree.lua
+++ lua/plugins/setup/nvim-tree.lua
@@ -69,6 +69,7 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = "e", action = "" }
       },
     },
   },
