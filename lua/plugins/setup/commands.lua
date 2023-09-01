172f6d75e18df95f53fe73d14b36c7d72aee6210
yidadaa <yidadaa@qq.com>
2023-09-01 11:01:25 +0800

    feat: upgrade deps

--- lua/plugins/setup/commands.lua
+++ lua/plugins/setup/commands.lua
@@ -1,6 +1,6 @@
 local telescope = require('telescope')
 
-local command_center = require("command_center")
+local command_center = require("commander")
 local noremap = { noremap = true }
 local silent_noremap = { noremap = true, silent = true }
 
@@ -12,31 +12,31 @@ function add_command(desc, cmd)
 end
 
 local commands = {
-  { "Git: 查看 commits",                          ":Telescope git_commits<cr>" },
-  { "Git: 查看 branches",                         ":Telescope git_branches<cr>" },
+  { "Git: 查看 commits", ":Telescope git_commits<cr>" },
+  { "Git: 查看 branches", ":Telescope git_branches<cr>" },
   { "Git: 查看 git 文件状态（git status）", ":Telescope git_status<cr>" },
-  { "Git: 查看文件变更 split view",           ":DiffviewOpen<cr>" },
-  { "Git: 刷新文件变更 split view",           ":DiffviewRefresh<cr>" },
-  { "Git: 查看当前文件历史记录",          ":DiffviewFileHistory<cr>" },
-  { "Git: 查看当前行改动记录",             ":CocCommand git.showBlameDoc<cr>" },
-  { "Git: 查看当前行 Commit 记录",           ":CocCommand git.showCommit<cr>" },
-  { "CMake: configure",                             ":CMake<cr>" },
-  { "CMake: build target",                          ":CMake build<cr>" },
-  { "CMake: run target",                            ":CMake run<cr>" },
-  { "CMake: debug target",                          ":CMake debug<cr>" },
-  { "CMake: build and debug target",                ":CMake build_and_debug<cr>" },
-  { "CMake: select target",                         ":CMake select_target<cr>" },
-  { "CMake: select cmake kit",                      ":CMake select_kit<cr>" },
-  { "CMake: select build type",                     ":CMake select_build_type<cr>" },
-  { "CMake: clean cache",                           ":CMake clear_cache<cr>" },
-  { "CMake: cmake create project",                  ":CMake create_project<cr>" },
-  { "CMake: cmake cancel",                          ":CMake cancel<cr>" },
-  { "Jest: 运行当前测试文件",               ":JestRun<cr>" },
-  { "Telescope: 全部命令",                      ":Telescope<cr>" },
-  { "Coc: 全部 coc 选项",                       ":Telescope coc<cr>" },
-  { "Coc Command: 全部命令",                    ":Telescope coc commands<cr>" },
-  { "Eslint: Restart Eslint server",                ":CocCommand eslint.restart<cr>" },
-  { "TS: Restart TS server",                        ":CocCommand tsserver.restart<cr>" },
+  { "Git: 查看文件变更 split view", ":DiffviewOpen<cr>" },
+  { "Git: 刷新文件变更 split view", ":DiffviewRefresh<cr>" },
+  { "Git: 查看当前文件历史记录", ":DiffviewFileHistory<cr>" },
+  { "Git: 查看当前行改动记录", ":CocCommand git.showBlameDoc<cr>" },
+  { "Git: 查看当前行 Commit 记录", ":CocCommand git.showCommit<cr>" },
+  { "CMake: configure", ":CMake<cr>" },
+  { "CMake: build target", ":CMake build<cr>" },
+  { "CMake: run target", ":CMake run<cr>" },
+  { "CMake: debug target", ":CMake debug<cr>" },
+  { "CMake: build and debug target", ":CMake build_and_debug<cr>" },
+  { "CMake: select target", ":CMake select_target<cr>" },
+  { "CMake: select cmake kit", ":CMake select_kit<cr>" },
+  { "CMake: select build type", ":CMake select_build_type<cr>" },
+  { "CMake: clean cache", ":CMake clear_cache<cr>" },
+  { "CMake: cmake create project", ":CMake create_project<cr>" },
+  { "CMake: cmake cancel", ":CMake cancel<cr>" },
+  { "Jest: 运行当前测试文件", ":JestRun<cr>" },
+  { "Telescope: 全部命令", ":Telescope<cr>" },
+  { "Coc: 全部 coc 选项", ":Telescope coc<cr>" },
+  { "Coc Command: 全部命令", ":Telescope coc commands<cr>" },
+  { "Eslint: Restart Eslint server", ":CocCommand eslint.restart<cr>" },
+  { "TS: Restart TS server", ":CocCommand tsserver.restart<cr>" },
 }
 
 for _, cmd in pairs(commands) do
--- lua/plugins/setup/treesitter.lua
+++ lua/plugins/setup/treesitter.lua
@@ -18,12 +18,13 @@ configs.setup({
 		"css",
 		"rust",
 		"vue",
-		"bash"
+		"bash",
+		"prisma",
+		"java"
 	},                      -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true,        -- false will disable the whole extension
-		disable = { "help" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
